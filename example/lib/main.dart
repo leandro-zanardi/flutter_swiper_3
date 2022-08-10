52132458dd90d7972b6c7eb09b8b837ce28a4f5a
Leandro Zanardi <lezanardi@gmail.com>
2022-08-10 13:46:56 -0300

    rename conflict

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper_3/flutter_swiper_null_safety.dart';
+import 'package:flutter_swiper_3/flutter_swiper_3.dart';
 
 import 'src/ExampleCustom.dart';
 import 'src/ExampleSwiperInScrollView.dart';
